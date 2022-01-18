0a2e9a3c0908b1f6be13814f7b02f7a6f5ea79de
jyuesong <425698907@qq.com>
2022-01-18 18:59:55 +0800

    Update extension.dart

--- lib/utils/extension.dart
+++ lib/utils/extension.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:qinglong_app/main.dart';
 
-//不推荐使用, 性能为o(n)
 extension ContextExt on BuildContext {
-  static QlAppState? _state;
-
   T read<T>(ProviderBase<T> provider) {
-    _state ??= findRootAncestorStateOfType<QlAppState>();
-    return _state!.ref.read(provider);
-  }
-
-  T watch<T>(ProviderBase<T> provider) {
-    _state ??= findRootAncestorStateOfType<QlAppState>();
-    return _state!.ref.watch(provider);
+    return ProviderScope.containerOf(this, listen: false).read(provider);
   }
 }
 

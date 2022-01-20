4f8285fcf1942033bcc2c56d3d79faa645757c41
jyuesong <425698907@qq.com>
2022-01-20 11:27:44 +0800

    Update token_interceptor.dart

--- lib/base/http/token_interceptor.dart
+++ lib/base/http/token_interceptor.dart
@@ -13,7 +13,7 @@ class TokenInterceptor extends Interceptor {
           "Bearer " + getIt<UserInfoViewModel>().token!;
     }
 
-    options.headers["User-Agent"] = "Dart/2.15.1(dart:io)";
+    options.headers["User-Agent"] = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
 
     options.headers["Content-Type"] = "application/json;charset=UTF-8";
     if (options.path != Url.loginByClientId) {
